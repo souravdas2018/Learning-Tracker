a5c2c3b7e6e9e149787c19022fe7b75765ce6227
souravdas2018 <souravdas20132014@gmail.com>
2026-01-11 09:10:10 +0530

    Dashboard

--- backend/src/routes/dashboard.routes.js
+++ backend/src/routes/dashboard.routes.js
@@ -3,6 +3,7 @@ const authMiddleware = require("../middlewares/auth.middleware");
 const adminMiddleware = require("../middlewares/admin.middleware");
 const dashboardController = require("../controllers/dashboard.controller");
 
+
 router.get("/", authMiddleware, dashboardController.getDashboard); //  ==> User Dashboard
 router.get("/", adminMiddleware); //  ==> Admin Dashboard
 
